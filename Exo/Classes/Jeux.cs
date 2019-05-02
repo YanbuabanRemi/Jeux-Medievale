e69f52f07d6939aca1fe86901812086147cebe62
Yanbuaban <remi.yanbuaban@gmail.com>
2019-05-02 16:54:31 +0200

    Ajout des fonctions nouvelle parties et charger parties

---
+++ Exo/Classes/Jeux.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exo.Classes
+{
+    class Jeux
+    {
+        private string nom;
+
+        
+
+        public string Nom { get => nom; }
+
+        public Jeux(string n, Royaume r)
+        {
+            nom = n;
+        }
+
+        
+
+        public void JeuxMenu()
+        {
+            
+
+            Console.Clear();
+
+
+            int choix;
+            do
+            {
+                //Console.Clear();
+                Console.WriteLine("---------------------------------Menu-------------------------------------\n");
+                Console.WriteLine("1 - Acheter un village (350PO)\n2 - Acheter un paysan (50PO)\n3 - Prélever les impôts\n4 - Fin de Tour\n0 - Quitter le jeux\n");
+                Console.WriteLine(R1.ToString() + "\n");
+                choix = Convert.ToInt32(Console.ReadLine());
+                switch (choix)
+                {
+                    case 1:
+                        Console.Clear();
+                        R1.AcheterVillage();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        R1.AcheterVillgeois();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        R1.PreleverImpot();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        R1.Vieillir();
+
+                        break;
+                    default:
+                        Console.WriteLine("Error 404");
+                        break;
+
+                }
+
+
+            } while (choix != 0);
+
+            Console.ReadLine();
+        }
+    }
+}
--- Exo/Program.cs
+++ Exo/Program.cs
@@ -1,58 +1,82 @@
 ﻿using Exo.Classes;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Exo
 {
     class Program
     {
+        public static List<Jeux> listeJeux = new List<Jeux>();
+
         static void Main(string[] args)
         {
-            Console.BackgroundColor = ConsoleColor.Gray;
-            Console.ForegroundColor = ConsoleColor.DarkMagenta;
-            Console.WriteLine("-----------------------------------------Bienvenue sur le super jeux Médiévale---------------------------------\n\n");
-            Console.WriteLine("-------------------------------------------Quel est le nom de votre Royaume ?----------------------------------\n");
-            Royaume R1 = new Royaume(Console.ReadLine());
-
-            Console.Clear();
             
 
-            int choix;
-            do
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+            Console.WriteLine("-----------------------------------------Bienvenue sur le super jeux Médiévale---------------------------------\n\n");
+
+            Console.WriteLine("1 - Nouvelle partie\n2 - Charger un partie\n3 - Quitter");
+            int choix = Convert.ToInt32(Console.ReadLine());
+
+            switch (choix)
             {
-                //Console.Clear();
-                Console.WriteLine("---------------------------------Menu-------------------------------------\n");
-                Console.WriteLine("1 - Acheter un village (350PO)\n2 - Acheter un paysan (50PO)\n3 - Prélever les impôts\n4 - Fin de Tour\n0 - Quitter le jeux\n");
-                Console.WriteLine(R1.ToString() + "\n");
-                choix = Convert.ToInt32(Console.ReadLine());
-                switch (choix)
-                {
-                    case 1:
-                        Console.Clear();                       
-                        R1.AcheterVillage();
-                        break;
-                    case 2:
-                        Console.Clear();
-                        R1.AcheterVillgeois();
-                        break;
-                    case 3:
-                        Console.Clear();
-                        R1.PreleverImpot();
-                        break;
-                    case 4:
-                        Console.Clear();
-                        R1.Vieillir();
-                        
-                        break;
-                    default:
-                        Console.WriteLine("Error 404");
-                        break;
-
-                }
+                case 1:
+                    NewGame();
+                    break;
+                case 2:
+                    ContinuGame();
+                    break;
+                case 3:
+                    Console.WriteLine("Error 404");
+                    break;
                 
-                
-            } while (choix != 0);
-
+            }
             Console.ReadLine();
+            
+        }
+
+        static void NewGame()
+        {
+            Console.WriteLine("Quel est le nom de votre partie ?");
+            string PartiName = Console.ReadLine();
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+            Console.WriteLine("-------------------------------------------Quel est le nom de votre Royaume ?----------------------------------\n");
+            Royaume R1 = new Royaume(Console.ReadLine());
+            Jeux j = new Jeux(PartiName, R1);
+            Console.Clear();
+            listeJeux.Add(j);
+            StreamWriter writer = new StreamWriter("Jeux.txt");
+            writer.WriteLine(JsonConvert.SerializeObject(listeJeux));
+            writer.Close();
+            j.JeuxMenu();
+        }
+
+        static void ContinuGame()
+        {
+            Console.WriteLine("Quel partie souhaitez-vous continuer ?");
+            List<Jeux> listeJeuxFromJson = new List<Jeux>();
+            if (File.Exists("Jeux.txt"))
+            {
+                StreamReader reader = new StreamReader("Jeux.txt");
+                listeJeuxFromJson = JsonConvert.DeserializeObject<List<Jeux>>(reader.ReadToEnd());
+                reader.Close();
+            }
+            foreach(Jeux j in listeJeuxFromJson)
+            {
+                Console.WriteLine(j.Nom);
+            }
+            string choixGame = Console.ReadLine();
+            foreach (Jeux j in listeJeuxFromJson)
+                {
+                if (choixGame == j.Nom)
+                    j.JeuxMenu();
+                }
         }
     }
 }
